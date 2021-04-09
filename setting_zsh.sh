cfb3f28f04d909368224fa2b5117104dd0bacb6f
washinson <sanya112001@yandex.ru>
2021-04-09 17:57:02 +0300

    Create setting_zsh.sh

---
+++ setting_zsh.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+## TODO: CHECK ONLY MAC OS, CHECK LINUX PLS
+# activate plugins
+sed -i '' 's/plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting /' ~/.zshrc
+
+# change theme
+sed -i '' 's/ZSH_THEME=".*"/ZSH_THEME="agnoster"/' ~/.zshrc
